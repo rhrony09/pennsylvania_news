4f110d8be52005d3e417e34e35cdd4d6aa2af22c
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-03-02 23:48:18 +0600

    dd check

--- app/View/Components/alert.php
+++ app/View/Components/alert.php
@@ -15,6 +15,7 @@ class Alert extends Component {
      * Create a new component instance.
      */
     public function __construct($icon = '', $text = '') {
+        dd('here');
         $this->icon = $icon;
         $this->text = $text;
     }
