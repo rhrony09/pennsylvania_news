b40290e10acf40e34e91b71331b381f956de1b08
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-03-03 00:37:25 +0600

    file component fixed

--- resources/views/components/forms/file.blade.php
+++ resources/views/components/forms/file.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->merge(['class' => 'mt-3']) }}>
     <x-forms.label :fieldId="$fieldId" :fieldLabel="$fieldLabel" fieldRequired="{{ isset($fieldRequired) ? $fieldRequired : '' }}">
     </x-forms.label>
-    <input type="file" class="dropify" id="{{ $fieldId }}" name="{{ $fieldName }}" data-default-file="{{ isset($fieldValue) ? $fieldValue : '' }}" @isset($allowedFileExtensions) data-allowed-file-extensions="{{ $allowedFileExtensions }}" @endisset data-height="{{ $fieldHeight }}" @isset($fieldRequired) required @endisset />
+    <input type="file" class="dropify" id="{{ $fieldId }}" name="{{ $fieldName }}" data-default-file="{{ isset($fieldValue) ? $fieldValue : '' }}" @isset($allowedFileExtensions) data-allowed-file-extensions="{{ $allowedFileExtensions }}" @endisset data-height="{{ isset($fieldHeight) ? $fieldHeight : '' }}" @isset($fieldRequired) required @endisset />
 
     @isset($fieldHelp)
         <small id="{{ $fieldId }}Help" class="form-text text-muted">{{ $fieldHelp }}</small>
